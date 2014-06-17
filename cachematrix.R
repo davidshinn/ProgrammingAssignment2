7315cb0a4853449acfc487586f634b6b05fcdf01
David Shinn <david.shinn@gmail.com>
2014-06-16 22:16:10 -0400

    Add working code for cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -9,8 +9,19 @@
 ## of an anonymous matrix and retrieving and calculating the inverse
 ## of the anonymous matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(this_matrix=matrix()) {
+    inverse_matrix <- NULL
+    set <- function(x) {
+        this_matrix <<- x
+        inverse_matrix <<- NULL # Remove cache when matrix is set
+    }
+    get <- function() this_matrix
+    set_inverse <- function(y) inverse_matrix <<- y
+    get_inverse <- function() inverse_matrix
+    list(set=set,
+         get=get,
+         set_inverse=set_inverse,
+         get_inverse=get_inverse)
 }
 
 
@@ -21,6 +32,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## the first time, and retrieving the stored value on
 ## any subsequent call
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cache_matrix, ...) {
+    inverse <- cache_matrix$get_inverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- cache_matrix$get()
+    inverse <- solve(data, ...)
+    cache_matrix$set_inverse(inverse)
+    inverse
 }
