6e95933f8d86beec327be78c8bcb90cc8ffee070
David Shinn <david.shinn@gmail.com>
2014-06-16 21:58:01 -0400

    Revise file and function comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions provides
+## persistence storage of the calculated inverse
+## of a matrix, so that if the inverse is called more than once,
+## it will return the stored value rather
+## than solve the inverse
 
-## Write a short comment describing this function
+## This function returns a list of functions
+## that provide retrieving and setting the value
+## of an anonymous matrix and retrieving and calculating the inverse
+## of the anonymous matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the matrix
+## accessible through a matrix created by makeCacheMatrix.
+## This optimizes the time retrieval of the inverse
+## by calculating the inverse and storing the value
+## the first time, and retrieving the stored value on
+## any subsequent call
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
